6b780d274d9905cd721a866be245d50dd0c6d072
unknown <muzamilkhan.26.10thc@gmail.com>
2024-07-28 18:11:50 +0530

    fifth commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
-// import React from 'react'
 import React,{useState} from "react";
 import image from "../assets/logo.png";
 import "./navbar.css";
+import secons from '../assets/icons8-menu-50.png'
 // import App from '../App';
 // import props from 'react';
 import { NavLink } from "react-router-dom";
@@ -16,15 +16,15 @@ function Navbar({searchanime,setsearchanime,getanimesearch,getsearch}) {
       }
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-light box-shawdow">
   <div className="container-fluid">
   <NavLink className="navbar-brand" to="/"><img src={image} alt="Logo" className="image"/></NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <img src={secons} alt="" className="navbar-toggler-icon"/>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        <li className="nav-item active">
         <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
         </li>
         <li className="nav-item">
@@ -41,7 +41,7 @@ function Navbar({searchanime,setsearchanime,getanimesearch,getsearch}) {
           </ul>
         </li>
         <li className="nav-item">
-        <NavLink className="nav-link" to="/contact" tabIndex="-1" aria-disabled="true">Contact Us</NavLink>
+        <NavLink className="nav-link" to="/contact" aria-disabled="true">Contact Us</NavLink>
         </li>
       </ul>
       <form className="d-flex" onSubmit={getanimesearch}>
@@ -56,3 +56,64 @@ function Navbar({searchanime,setsearchanime,getanimesearch,getsearch}) {
 }
 
 export default Navbar;
+
+
+// import React,{useState} from "react";
+// import image from "../assets/logo.png";
+// import "./navbar.css";
+// // import App from '../App';
+// // import props from 'react';
+// import { NavLink } from "react-router-dom";
+// import { accordionActionsClasses } from "@mui/material";
+
+// function Navbar({searchanime,setsearchanime,getanimesearch,getsearch}) {
+//   // const [three,setthree]=useState([]);
+
+//   const actionn=(e)=>{
+//     console.log(e);
+
+//     getsearch;
+//   }
+//   return (
+//     <>
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light box-shawdow">
+//   <div className="container-fluid">
+//     <NavLink className="navbar-brand" to="/"><img src={image} alt="Logo" className="image"/></NavLink>
+//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+//       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//         <li className="nav-item">
+//           <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink className="nav-link" to="/about">About Us</NavLink>
+//         </li>
+//         <li className="nav-item dropdown">
+//           <NavLink className="nav-link dropdown-toggle" to="/genre" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Genres
+//           </NavLink>
+//           <ul className="dropdown-menu droppp" aria-labelledby="navbarDropdown" onClick={(e)=>{{actionn(e.target.innerHTML.toLowerCase())}}}>
+//             <li><NavLink className="dropdown-item" to="/genre/action">Action</NavLink></li>
+//             <li><NavLink className="dropdown-item" to="/genre/adventure">Adventure</NavLink></li>
+//             <li><NavLink className="dropdown-item" to="/genre/comedy">Comedy</NavLink></li>
+//           </ul>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink className="nav-link" to="/contact" tabIndex="-1" aria-disabled="true">Contact Us</NavLink>
+//         </li>
+//       </ul>
+      
+//     </div>
+//     <form className="d-flex" onSubmit={getanimesearch}>
+//         <input className="form-control me-2" type="search" placeholder="Search for an Anime..." aria-label="Search" value={searchanime} onChange={(e)=>{setsearchanime(e.target.value)}}/>
+//         <button className="btn btn-outline-success" type="submit" >Search</button>
+//       </form>
+//   </div>
+// </nav>
+//     </>
+//   );
+// }
+
+// export default Navbar;
---
+++ src/Components/readme.md
@@ -0,0 +1,59 @@
+// import React,{useState} from "react";
+// import image from "../assets/logo.png";
+// import "./navbar.css";
+// // import App from '../App';
+// // import props from 'react';
+// import { NavLink } from "react-router-dom";
+// import { accordionActionsClasses } from "@mui/material";
+
+// function Navbar({searchanime,setsearchanime,getanimesearch,getsearch}) {
+//   // const [three,setthree]=useState([]);
+
+//   const actionn=(e)=>{
+//     console.log(e);
+
+//     getsearch;
+//   }
+//   return (
+//     <>
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light box-shawdow">
+//   <div className="container-fluid">
+//     <NavLink className="navbar-brand" to="/"><img src={image} alt="Logo" className="image"/></NavLink>
+//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//         <li className="nav-item">
+//           <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink className="nav-link" to="/about">About Us</NavLink>
+//         </li>
+//         <li className="nav-item dropdown">
+//           <NavLink className="nav-link dropdown-toggle" to="/genre" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Genres
+//           </NavLink>
+//           <ul className="dropdown-menu droppp" aria-labelledby="navbarDropdown" onClick={(e)=>{{actionn(e.target.innerHTML.toLowerCase())}}}>
+//             <li><NavLink className="dropdown-item" to="/genre/action">Action</NavLink></li>
+//             <li><NavLink className="dropdown-item" to="/genre/adventure">Adventure</NavLink></li>
+//             <li><NavLink className="dropdown-item" to="/genre/comedy">Comedy</NavLink></li>
+//           </ul>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink className="nav-link" to="/contact" tabIndex="-1" aria-disabled="true">Contact Us</NavLink>
+//         </li>
+//       </ul>
+      
+//     </div>
+//     <form className="d-flex" onSubmit={getanimesearch}>
+//         <input className="form-control me-2" type="search" placeholder="Search for an Anime..." aria-label="Search" value={searchanime} onChange={(e)=>{setsearchanime(e.target.value)}}/>
+//         <button className="btn btn-outline-success" type="submit" >Search</button>
+//       </form>
+//   </div>
+// </nav>
+//     </>
+//   );
+// }
+
+// export default Navbar;
